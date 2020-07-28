9ad776c321e682420d5ae9347aac938efce8bb18
Guilherme Sampaio <santos.gsampaio@gmail.com>
2020-07-28 17:14:10 -0300

    Upload

---
+++ AspNetCore.MassTransit/AspNetCore.MassTransit/Controllers/WeatherForecastController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AspNetCore.MassTransit.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IBus _bus;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IBus bus)
+        {
+            _logger = logger;
+            _bus = bus;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] RegionCreated region)
+        {
+            _logger.LogInformation($"Region created {region}");
+
+            await _bus.Publish<RegionCreated>(region);
+
+            return Accepted();
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+
+
+
+    }
+
+    public class RegionCreated
+    {
+        public RegionCreated()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ AspNetCore.MassTransit/AspNetCore.MassTransit/FixedIncomeChangedConsumer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace AspNetCore.MassTransit
+{
+    internal class FixedIncomeChangedConsumer : IConsumer<FixedIncomeChangedEvent>
+    {
+        private readonly ILogger<FixedIncomeChangedConsumer> _logger;
+
+        public FixedIncomeChangedConsumer(ILogger<FixedIncomeChangedConsumer> logger)
+        {
+            _logger = logger;
+        }
+        public Task Consume(ConsumeContext<FixedIncomeChangedEvent> context)
+        {
+            _logger.LogInformation($"Instrument received: {JsonConvert.SerializeObject(context.Message)}");
+            return Task.CompletedTask;
+        }
+    }
+
+    internal class FixedIncomeChangedEvent
+    {
+        public int ProductType { get; set; }
+        public int OperationType { get; set; }
+        public int ProductId { get; set; }
+        public DateTime RequestDate { get; set; }
+        public Guid EventId { get; set; }
+        public string SyncKey { get; set; }
+
+    }
+}
